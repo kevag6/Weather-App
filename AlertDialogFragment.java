e328439a509e0f819db350f341bd97968e4cae88
Kevin, A, Grifo <grifokevin@gmail.com>
2018-06-06 09:21:47 -0400

    Add files via upload

---
+++ AlertDialogFragment.java
@@ -0,0 +1,21 @@
+package com.teamtreehouse.stormy;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.os.Bundle;
+
+public class AlertDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Context context = getActivity();
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+
+        builder.setTitle("Oops! Sorry")
+        .setMessage("There was an error please try again")
+        .setPositiveButton("OK", null);
+
+        return builder.create();
+    }
+}
---
+++ MainActivity.java
@@ -0,0 +1,181 @@
+package com.teamtreehouse.stormy;
+
+import android.content.Context;
+import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.util.JsonReader;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.teamtreehouse.stormy.databinding.ActivityMainBinding;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+    private CurrentWeather currentWeather;
+
+    private ImageView iconImageView;
+
+    final double latitude = 37.8267;
+    final double longitude = -122.4233;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getForecast(latitude, longitude);
+
+        Log.d(TAG, "Main UI code is running, yaaas!");
+    }
+
+    private void getForecast(double latitude, double longitude) {
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(MainActivity.this, R.layout.activity_main);
+
+        TextView darkSky = findViewById(R.id.darkSkyAttribution);
+
+        darkSky.setMovementMethod(LinkMovementMethod.getInstance());
+
+        //initialize image view
+        iconImageView = findViewById(R.id.iconImageView);
+
+        String apiKey = "9a37f955886e746ce704ade97295480e";
+        //
+
+
+        String forecastURL = "https://api.darksky.net/forecast/"
+                + apiKey + "/" + latitude + "," + longitude;
+
+        if(isNetworkAvailable()) {
+
+            OkHttpClient client = new OkHttpClient();
+
+            Request request = new Request.Builder()
+                    .url(forecastURL)
+                    .build();
+
+            Call call = client.newCall(request);
+
+            call.enqueue(new Callback() {
+                @Override
+                public void onFailure(Call call, IOException e) {
+
+                }
+
+                @Override
+                public void onResponse(Call call, Response response) throws IOException {
+                    try {
+                        String jsonData = response.body().string();
+                        Log.v(TAG, jsonData);
+                        if (response.isSuccessful()) {
+                            currentWeather = getCurrentDetails(jsonData);
+
+                            final CurrentWeather displayWeather = new CurrentWeather(
+                                    currentWeather.getLocationLabel(),
+                                    currentWeather.getIcon(),
+                                    currentWeather.getTime(),
+                                    currentWeather.getTemperature(),
+                                    currentWeather.getHumidity(),
+                                    currentWeather.getPrecipChance(),
+                                    currentWeather.getSummary(),
+                                    currentWeather.getTimeZone()
+                            );
+
+                            binding.setWeather(displayWeather);
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Drawable drawable = getResources().getDrawable(displayWeather.getIconId());
+                                    iconImageView.setImageDrawable(drawable);
+                                }
+                            });
+
+                        } else {
+                            alertUserAboutError();
+
+                        }
+                    } catch (IOException e) {
+                        Log.e(TAG, "IO Exception caught: ", e);
+                        e.printStackTrace();
+                    }
+                    catch(JSONException e){
+                        Log.e(TAG, "IO Exception caught: ", e);
+                        e.printStackTrace();
+                    }
+                }
+            });
+        }
+    }
+
+    private CurrentWeather getCurrentDetails(String jsonData) throws JSONException{
+        JSONObject forecast = new JSONObject(jsonData);
+
+        String timezone = forecast.getString("timezone");
+        Log.i(TAG, "From JSON: " + timezone);
+        //currentWeather.set
+        JSONObject currently = forecast.getJSONObject("currently");
+
+        CurrentWeather currentWeather = new CurrentWeather();
+
+        currentWeather.setHumidity(currently.getDouble("humidity"));
+        currentWeather.setTime(currently.getLong("time"));
+        currentWeather.setSummary(currently.getString("summary"));
+        currentWeather.setIcon(currently.getString("icon"));
+        currentWeather.setLocationLabel("Alcatraz Island, CA");
+        currentWeather.setPrecipChance(currently.getDouble("precipProbability"));
+        currentWeather.setTemperature(currently.getDouble("temperature"));
+        currentWeather.setTimeZone(timezone);
+
+        Log.d(TAG, currentWeather.getFormattedTime());
+
+        return currentWeather;
+    }
+
+    //Checks to see if the network is currently available or not.
+    private boolean isNetworkAvailable() {
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        boolean isAvailable = false;
+        if(networkInfo != null && networkInfo.isConnected()){
+            isAvailable = true;
+        }
+        else{ //Let's the user know via a toast message that the network is unavailable
+            //Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_LONG).show();
+            AlertDialogFragment dialog = new AlertDialogFragment();
+            dialog.show(getFragmentManager(), "error_dialog");
+        }
+        return isAvailable;
+    }
+
+    //
+    private void alertUserAboutError() {
+        AlertDialogFragment dialog = new AlertDialogFragment();
+
+        dialog.show(getFragmentManager(), "error_dialog");
+
+    }
+
+
+    public void refreshOnClick(View view){
+        Toast.makeText(this, "Refreshing Data", Toast.LENGTH_LONG).show();
+        getForecast(latitude, longitude);
+    }
+}
